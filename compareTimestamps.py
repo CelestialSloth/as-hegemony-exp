0dff379e89cefc8cb388e08f8b8be88cb8cf9884
Elaine Demetrion <esdemetrion@gmail.com>
2023-12-04 10:58:37 -0500

    File to check timestamps of kafka messages

---
+++ compareTimestamps.py
@@ -0,0 +1,44 @@
+import sys
+import msgpack
+import argparse
+import csv
+import os
+
+from confluent_kafka import Consumer, TopicPartition, KafkaError
+from collections import defaultdict
+
+def compareTimestamps(topic):
+    timestamps = defaultdict(lambda: 0)
+
+    # populate the timestamps dictionary
+    while True:
+        msg = consumer.poll(1000)
+        msgdict = {
+            'topic': msg.topic(),
+            'partition': msg.partition(),
+            'key': msg.key(),
+            'timestamp': msg.timestamp(),
+            'headers': msg.headers(),
+            'value': msgpack.unpackb(msg.value(), raw=False)
+        }
+
+        data = msgdict['value']
+        timestamps[data.timestamp] += 1
+
+        i += 1
+        if i >= high:
+            break
+
+    consumer.close()
+
+    for timestamp in timestamps.keys():
+        seconds = int(timestamp) / 1000
+        days = seconds / (24 * 60 * 60)
+        console.log(str(days) + " days ago")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--topic', default='ihr_bgp_atom_route-views2')
+    args = parser.parse_args()
+
+    compareTimestamps(args.topic)
